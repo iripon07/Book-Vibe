4c9a48c8dc81a9d75cf52e84e42097423fb53d6b
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-08-31 15:55:47 +0600

    Banner added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
 
 const Home = () => {
-  return <div className="max-w-7xl mx-auto"></div>;
-}
+  return (
+    <div>
+      <Banner></Banner>
+      <Books></Books>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
